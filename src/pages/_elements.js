206071f89885ed8d8a1586cb2f8fe41b655400c3
najdek <mateusz@najdek.me>
2024-03-14 15:15:50 +0100

    Added footer, added dark mode

--- src/pages/_elements.js
+++ src/pages/_elements.js
@@ -1,33 +1,47 @@
 import { Navbar } from "flowbite-react";
+import { Footer } from 'flowbite-react';
 
-export function Header() {
-    return (
-      <Navbar fluid={true} rounded={true}>
+import { Cabin } from "next/font/google";
+const cabin = Cabin({ subsets: ["latin"] });
+
+
+export function NavbarElement() {
+  return (
+      <Navbar className={`${cabin.className}`} fluid={true} rounded={true}>
         <Navbar.Brand href="/">
-          <img src="/logo.svg" className="mr-3 h-6 sm:h-9"></img>
-  
+          <div className="mr-3 h-6 sm:h-9 monmat-logo"></div>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
-      <Navbar.Link
-        href="/"
-        active={true}
-      >
-        Home
-      </Navbar.Link>
-      <Navbar.Link href="/about-us">
-        About Us
-      </Navbar.Link>
-      <Navbar.Link href="/products">
-        Products
-      </Navbar.Link>
-      <Navbar.Link href="/gallery">
-        Gallery
-      </Navbar.Link>
-      <Navbar.Link href="/contact-us">
-        Contact Us
-      </Navbar.Link>
-    </Navbar.Collapse>
+          <Navbar.Link
+            href="/"
+            active={true}
+          >
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/about-us">
+            About Us
+          </Navbar.Link>
+          <Navbar.Link href="/products">
+            Products
+          </Navbar.Link>
+          <Navbar.Link href="/gallery">
+            Gallery
+          </Navbar.Link>
+          <Navbar.Link href="/contact-us">
+            Contact Us
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
+  )
+}
+
+export function FooterElement() {
+  return (
+    <Footer className={cabin.className} container>
+      <div className="w-full text-center">
+        <Footer.Copyright href="#" by="MONMAT Mateusz Najdek™" year={2024} />
+      </div>
+    </Footer>
     )
-  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import Image from "next/image";
+import { NavbarElement } from "./_elements";
+import { FooterElement } from "./_elements";
+
 import { Cabin } from "next/font/google";
 const cabin = Cabin({ subsets: ["latin"] });
-import { Header } from "./_elements";
-
-
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <NavbarElement />
+
+
       <main
-        className={`flex min-h-screen flex-col items-center ${cabin.className}`}
+       className={`flex min-h-screen flex-col items-center ${cabin.className}`}
       >
-        <div>Hello</div>
+        hey
       </main>
+
+      <FooterElement />
     </>
 
   );
